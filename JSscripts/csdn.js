ff469e08ba2fa6426ab48394b98ecf845fda70f9
liang <1035847702@qq.com>
2021-05-09 18:26:28 +0800

    init

---
+++ JSscripts/csdn.js
@@ -0,0 +1,24 @@
+const fetch = require("node-fetch")
+
+var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+};
+
+fetch("https://blog.csdn.net/community/home-api/v1/get-business-list?page=1&size=500&businessType=blog&orderby=&noMore=false&username=fanqiliang630", requestOptions)
+    .then(response => response.text())
+    .then(result => {
+        var res = JSON.parse(result);
+        res = res.data.list;
+        res = res.map(item => item.articleId)
+
+        var i = 0;
+        var intervaler = setInterval(() => {
+            if(i >= res.length) {
+                clearInterval(intervaler)
+            }
+            fetch("https://blog.csdn.net/fanqiliang630/article/details/" + res[i]);
+            i += 1;
+        }, 400)
+    })
+    .catch(error => console.log('error', error));
