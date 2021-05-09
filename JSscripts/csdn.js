a1a4e5629a66224e06688c9f660416c49cc00b37
liang <1035847702@qq.com>
2021-05-09 19:55:09 +0800

    add readme

--- JSscripts/csdn.js
+++ JSscripts/csdn.js
@@ -5,22 +5,27 @@ var requestOptions = {
     redirect: 'follow'
 };
 
-fetch("https://blog.csdn.net/community/home-api/v1/get-business-list?page=1&size=500&businessType=blog&orderby=&noMore=false&username=fanqiliang630", requestOptions)
-    .then(response => response.text())
-    .then(result => {
-        var res = JSON.parse(result);
-        res = res.data.list;
-        res = res.map(item => item.articleId)
+async function main() {
+    var result = []
+    var promise = fetch("https://blog.csdn.net/community/home-api/v1/get-business-list?page=1&size=500&businessType=blog&orderby=&noMore=false&username=fanqiliang630", requestOptions)
+        .then(response => response.text())
+        .then(data => {
+            var res = JSON.parse(data);
+            res = res.data.list;
+            res = res.map(item => item.articleId)
+            result = res
+            return "success"
+        }).catch(error => {
+            console.log('error', error);
+            return "failed"
+        });
+    var promise_res = await promise;
+    console.log(promise_res)
 
-        var i = 0;
-        var intervaler = setInterval(() => {
-            if(i >= res.length) {
-                clearInterval(intervaler)
-            }
-            fetch("https://blog.csdn.net/fanqiliang630/article/details/" + res[i]);
-            i += 1;
-        }, 500)
-    })
-    .catch(error => {
-        console.log('error', error)
-    });
+    for (var i = 0; i < result.length; i++) {
+        var res = fetch("https://blog.csdn.net/fanqiliang630/article/details/" + result[i]);
+        await res;
+    }
+}
+
+main()
